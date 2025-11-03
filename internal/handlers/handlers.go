b59adcd058a5c3bed7a8f924084f1312e627d10d
Roman Zheglov <31963386+demderk@users.noreply.github.com>
2025-11-04 06:01:22 +0700

    Update internal/handlers/handlers.go
    Co-authored-by: Ivan Nizhnichenko <ivan_0805@mail.ru>

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -43,7 +43,7 @@ func PostUpload(response http.ResponseWriter, request *http.Request) {
 	file, _, err := request.FormFile("myFile")
 	if err != nil {
 		serverLogs.Main.Println(fmt.Errorf("form file raised an error: %w", err))
-		http.Error(response, "Internal server error", http.StatusInternalServerError)
+		http.Error(response, "Internal server error", http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
