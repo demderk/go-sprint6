5e016d1194792eb048da74f9c5a71f7a0830c656
Roman Zheglov <damdark107@gmail.com>
2025-11-04 03:30:40 +0700

    Sprint 6

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"go-sprint6/pkg/morse"
+	"strings"
+)
+
+func isMorse(text string) bool {
+	empty := strings.ReplaceAll(text, ".", "")
+	empty = strings.ReplaceAll(empty, "-", "")
+	empty = strings.ReplaceAll(empty, " ", "")
+
+	return len(empty) == 0
+}
+
+func ConvertText(text string) string {
+	if isMorse(text) {
+		return morse.ToText(text)
+	} else {
+		return morse.ToMorse(text)
+	}
+}
